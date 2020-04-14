5656d8a8185cb2d6f5a96cca21ae468d6d5547ba
Pch12_Projects <pch12.656@gmail.com>
2020-04-14 20:21:15 +0300

    Update and rename decode_powershell64.bash to decode64.bash

--- PowertoBase64/decode64.bash
+++ PowertoBase64/decode64.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwsh -v >/dev/null 2>&1 || { echo >&2 "I require PowerShell but it's not installed.Please install 'pwsh'  Aborting. "; exit 1;  }
-echo '-----@@@PowerShell Base64 Decryptor@@@-----'
+echo '-----PowerShell Base64 Decryptor-----'
 echo ''
 echo ''
 read -p "Enter command for base64: " command
